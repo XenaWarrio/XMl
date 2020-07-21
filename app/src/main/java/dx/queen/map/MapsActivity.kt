42f7f77fbd108b586e62ac911c3acfe0be067131
Xena <queen.de.xena@gmail.com>
2020-07-21 07:08:26 +0300

    Xml parsing error

---
+++ app/src/main/java/dx/queen/map/MapsActivity.kt
@@ -0,0 +1,60 @@
+package dx.queen.map
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private lateinit var mMap: GoogleMap
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_maps)
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        val mapFragment = supportFragmentManager
+                .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+
+        val networkDataSource =
+            NetworkDataSource(
+                retrofit.create(
+                    INetworkDataSource::class.java
+                )
+            )
+
+        GlobalScope.launch(Dispatchers.IO) {
+            val data = networkDataSource.getData()
+            Log.d("dnbfk"," $data")
+        }
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+
+        // Add a marker in Sydney and move the camera
+        val sydney = LatLng(-34.0, 151.0)
+        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+    }
+}
---
+++ app/src/main/java/dx/queen/map/TestExampleData.kt
@@ -0,0 +1,16 @@
+package dx.queen.map
+
+import org.simpleframework.xml.Element
+import org.simpleframework.xml.Root
+
+@Root(name = "TestExample", strict = false)
+data class TestExampleData @JvmOverloads constructor(
+    @field:Element(name = "Message", required = false)
+    @param:Element(name = "Message", required = false)
+    val message: String? = null,
+
+    @field:Element(name = "GPS", required = false)
+    @param:Element(name = "GPS", required = false)
+    val gps: String? = null
+)
+
